1dd975f4fdff9cb803658bbdf7b23747e4e0fdb6
IagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2023-06-27 11:55:51 -0300

    Criado componente Icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,16 @@ import styles from './Header.module.css'
 
 import Title from "../Title/Title"
 import Subtitle from "../subtitle/Subtitle"
+import Icon from '../Icon/Icon'
 
 function Header() {
     return(
         <div className={styles.header}>
             <Title>Jogo da Velha</Title>
             <Subtitle>Criado por Iago Pinheiro</Subtitle>
+            <div className={styles.iconContent}>
+                <Icon iconName="github" link="https://github.com/iagoPinheiro1401" />
+            </div>
         </div>
     )
 }
