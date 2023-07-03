43931e6eea14513fbd64f797743d9845b878a546
IagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2023-07-03 10:10:47 -0300

    Criado estados no GameOption

--- src/components/gameOptions/GameOptions.jsx
+++ src/components/gameOptions/GameOptions.jsx
@@ -1,8 +1,17 @@
 import styles from "./GameOptions.module.css"
+import Icon from "../Icon/Icon"
 
-function GameOption() {
+const GameIcon = ({ iconName }) => <Icon iconName={iconName} size="25px" />
+
+function GameOption({ status }) {
     return(
         <div className={styles.option}>
+            {
+                status === 1 && <GameIcon iconName="circle" />
+            }
+            {
+                status === -1 && <GameIcon iconName="x" />
+            }
         </div>
     )
 }
